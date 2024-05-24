70c83b1a3396e89bd84cf17a15dea0278f7c5c16
hangonn <nnhango@protonmail.com>
2024-05-24 11:59:31 +0200

    init

---
+++ src/main.rs
@@ -0,0 +1,64 @@
+use regex::Regex;
+use tokio::io::AsyncWriteExt;
+use tokio::task::JoinSet;
+
+#[tokio::main]
+async fn main() {
+    let pattern = r#"<a href="([^"]+)">"#;
+    let re = Regex::new(pattern).unwrap();
+    let url = "https://www.windhoekcc.org.na/documents/";
+    let client = reqwest::Client::new();
+    download(url, &client, &re).await;
+}
+
+async fn download(url: &'static str, client: &reqwest::Client, re: &Regex) {
+    let response = client.get(url).send().await.unwrap();
+    let body = response.text().await.unwrap();
+
+    let files = re
+        .captures_iter(&body)
+        .filter_map(|f| f.get(1).map(|f| f.as_str().to_string()))
+        .collect::<Vec<String>>();
+
+    let mut tasks = JoinSet::new();
+
+    for file in files {
+        if !file.ends_with(".pdf") {
+            continue;
+        }
+
+        let client = client.clone();
+        tasks.spawn(async move {
+            let file = file.split("/").last().unwrap();
+            let file = format!("./assets/{}", file);
+            let url = format!("{}{}", url, file);
+            let mut file = tokio::fs::File::create(file).await.unwrap();
+            let mut response = client.get(&url).send().await.unwrap();
+            while let Some(chunk) = response.chunk().await.unwrap() {
+                file.write_all(&chunk).await.unwrap();
+            }
+        });
+    }
+
+    while let Some(_) = tasks.join_next().await {}
+
+    // for cap in re.captures_iter(&body) {
+    //     if let Some(path) = cap.get(1) {
+    //         println!("{}", path.as_str());
+    //     }
+    // }
+
+    return;
+    // let document = Document::from(body.as_str());
+    // let links = document.find(Name("a")).filter_map(|n| n.attr("href"));
+    // for link in links {
+    //     let file = link.split("/").last().unwrap();
+    //     let file = format!("{}{}", output, file);
+    //     let url = format!("{}{}", url, link);
+    //     let mut file = File::create(file).unwrap();
+    //     let mut response = reqwest::get(url).await.unwrap();
+    //     while let Some(chunk) = response.chunk().await.unwrap() {
+    //         file.write_all(&chunk).unwrap();
+    //     }
+    // }
+}
