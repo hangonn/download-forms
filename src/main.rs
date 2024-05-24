3faf00b0f5a9ef8a6f9daa578ab25088afaef456
Ndeyapo <41614202+hangonn@users.noreply.github.com>
2024-05-24 12:35:31 +0000

    done done

--- src/main.rs
+++ src/main.rs
@@ -19,20 +19,21 @@ async fn download(url: &'static str, client: &reqwest::Client, re: &Regex) {
     let files = re
         .captures_iter(&body)
         .filter_map(|f| f.get(1).map(|f| f.as_str().to_string()))
+        .filter(|f| f.ends_with(".pdf"))
         .collect::<Vec<String>>();
 
     let mut tasks = JoinSet::new();
 
+    println!("Found {} files to download", files.len());
+
     for file in files {
-        if !file.ends_with(".pdf") {
-            continue;
-        }
 
         let client = client.clone();
         tasks.spawn(async move {
             let file = file.split("/").last().unwrap();
             let url = format!("{}{}", url, file);
-            let file = if file.contains("form") {
+            let file = file.replace("%20", " ");
+            let file = if file.to_ascii_lowercase().contains("form") {
                 format!("./assets/forms/{}", file)
             } else {
                 format!("./assets/{}", file)
@@ -40,10 +41,12 @@ async fn download(url: &'static str, client: &reqwest::Client, re: &Regex) {
             let mut file = tokio::fs::File::create(file).await.unwrap();
             println!("Downloading: {}", url);
             let mut response = client.get(&url).send().await.unwrap();
-            println!("Downlaoding: {}", url);
             while let Some(chunk) = response.chunk().await.unwrap() {
                 file.write_all(&chunk).await.unwrap();
             }
+            println!("Finished downloading: {}", url);
+            file.sync_all().await.unwrap();
+
         });
     }
 
